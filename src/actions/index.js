0051de916fd9c7e365f019047feb5430ebe1b0d3
unknown <rabbithole.th@gmail.com>
2021-02-07 05:16:05 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+//Action：typeをキーに持つオブジェクト
+//ActionCreator：actionを実行する関数
+export const increment = () => ({
+    type: INCREMENT
+})
+//ActionCreator
+export const decrement = () => ({
+    type: DECREMENT
+})
