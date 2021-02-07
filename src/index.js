a5ef69ce195c71f1edaf9495eed8ee33f83a68de
unknown <rabbithole.th@gmail.com>
2021-02-08 06:34:16 +0900

    update

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ import reducer from './reducers';
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
 
-//import * as serviceWorker from './serviceWorker';
-import registerServiceWorker from './registerServiceWorker'
+import * as serviceWorker from './serviceWorker';
+//import registerServiceWorker from './registerServiceWorker'
 
 // ミドルウェアとしてreact-thunkを許可する
 const store = createStore(reducer, applyMiddleware(thunk))
@@ -37,5 +37,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
-registerServiceWorker();
+serviceWorker.unregister();
+//registerServiceWorker();
