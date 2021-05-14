6ad9e3d010711cf58c13326da1e7c464a7a36587
rabbithole-th <rabbithole.th@gmail.com>
2021-05-14 12:16:11 +0900

    apply material-ui to eventsIndex

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { composeWithDevTools } from 'redux-devtools-extension'
+// Material-ui
+import MuiThereProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './index.css';
 import reducer from './reducers'
@@ -16,6 +18,7 @@ import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
 import EventsShow from './components/events_show';
 import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 //redux-devtoolsでデバッグできる状態にする
 const enhancer = process.env.NODE_ENV === 'development' ?
@@ -25,16 +28,18 @@ const store = createStore(reducer, enhancer)
 
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/events/new" component={EventsNew} />
-        <Route path="/events/:id" component={EventsShow} />
-        <Route exact path="/" component={EventsIndex} />
-        <Route exact path="/events" component={EventsIndex} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/events/new" component={EventsNew} />
+          <Route path="/events/:id" component={EventsShow} />
+          <Route exact path="/" component={EventsIndex} />
+          <Route exact path="/events" component={EventsIndex} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>    
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
 registerServiceWorker();
